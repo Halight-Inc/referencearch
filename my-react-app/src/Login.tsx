c1df14f46e35872dffcfda907f80904246caff90
shogg <shogg@halight.onmicrosoft.com>
2025-02-26 19:49:49 -0500

    Adding CI CD Pipeline with Docker

--- my-react-app/src/Login.tsx
+++ my-react-app/src/Login.tsx
@@ -2,7 +2,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/v1';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const Login: React.FC<{ onLogin: (token: string) => void }> = ({ onLogin }) => {
     const [username, setUsername] = useState('');
