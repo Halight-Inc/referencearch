c6b4cc4f4128d947e4211ed7d237d461430ffd94
shogg <shogg@halight.onmicrosoft.com>
2025-03-05 08:59:25 -0500

    pipeline changes

--- start.ps1
+++ start.ps1
@@ -1,6 +1,16 @@
-# filepath: /C:/code/referencearch/my-nodejs-api/start.ps1
-# Build the Docker image
+# Set the environment mode based on an argument passed to the script
+$env:ENV_MODE = "test"  # Default to development
+
+if ($args[0] -eq "test") {
+    $env:ENV_MODE = "test"
+} elseif ($args[0] -eq "production") {
+    $env:ENV_MODE = "production"
+}
+
+Write-Host "Using environment mode: $env:ENV_MODE"
+
+# Build Docker images with the correct environment mode
 docker-compose build
 
-# Run the Docker container
-docker-compose up --watch
+# Run Docker containers with the correct environment file
+docker-compose up --build
