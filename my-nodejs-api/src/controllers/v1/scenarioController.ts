54ea5f59d0c27e48fcc154f3d92aed8fcc55dd64
Jake Dennison <jake.dennison@halight.com>
2025-04-14 11:26:03 -0400

    Add getScenario to controller, add endpoint for fetching single scenario, add getAllScenarios and getScenario to api script

--- my-nodejs-api/src/controllers/v1/scenarioController.ts
+++ my-nodejs-api/src/controllers/v1/scenarioController.ts
@@ -1,16 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import db from '../../database';
 
-const getAllScenarios = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const scenarios = await db.CoachonCueScenario.findAll();
-        res.json(scenarios);
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-};
-
 const createScenario = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const newScenario = await db.CoachonCueScenario.create(req.body);
@@ -21,7 +11,29 @@ const createScenario = async (req: Request, res: Response, next: NextFunction) =
     }
 };
 
+const getScenario = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const scenarioId = req.params.scenarioId; // Access the ID here
+        const scenario = await db.CoachonCueScenario.findByPk(scenarioId);
+        res.status(200).json(scenario);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
+
+const getAllScenarios = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const scenarios = await db.CoachonCueScenario.findAll();
+        res.json(scenarios);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
+
 export default {
-    getAllScenarios,
     createScenario,
+    getScenario,
+    getAllScenarios,
 };
