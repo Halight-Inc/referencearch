665a4e61a9ad65e5f1c606d15787f27c65bc8fff
shogg <shogg@halight.onmicrosoft.com>
2025-04-16 15:14:17 -0400

    updates

--- my-nodejs-api/src/routes/v1/simulationRoutes.ts
+++ my-nodejs-api/src/routes/v1/simulationRoutes.ts
@@ -8,4 +8,9 @@ router.post('/', authenticateToken, simulationController.createSimulation);
 router.get('/', authenticateToken, simulationController.getAllSimulation);
 router.get('/:simulationId', authenticateToken, simulationController.getSimulation);
 
+// Add the new PATCH route for updates
+router.patch('/:simulationId', authenticateToken, simulationController.updateSimulation);
+// You could use PUT instead if you expect the client to send the *entire* resource representation
+// router.put('/:simulationId', authenticateToken, simulationController.updateSimulation);
+
 export default router;
