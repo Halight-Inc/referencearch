f25f35c9ff07ae42d3dda4cf3ff3feeeb5643963
shogg <shogg@halight.onmicrosoft.com>
2025-04-17 12:35:53 -0400

    consolidating changes

--- my-nodejs-api/src/server.ts
+++ my-nodejs-api/src/server.ts
@@ -3,22 +3,20 @@ import db from './database';
 import seed from './database/seed/seed';
 import 'newrelic';
 import * as http from 'http';
-import {Server} from 'socket.io';
-import {setServerInstance} from './serverInstance';
+import { Server } from 'socket.io';
+import { setServerInstance } from './serverInstance';
 import addRoutes from './app';
 import express from 'express';
 
 const MAX_RETRIES = config.dbRetries; // Maximum number of retries
 const RETRY_DELAY = 5000; // Delay between retries in milliseconds (5 seconds)
 let retryCount = 0;
-let databaseConnectionAvailable = false;
 
 const checkDatabaseConnection = async (): Promise<void> => {
   try {
     console.log('Checking database connection...');
     await db.sequelize.authenticate();
     console.log('Database connection successful.');
-    databaseConnectionAvailable = true; //update
   } catch (error) {
     console.error('Error connecting to the database:', error);
     if (retryCount < MAX_RETRIES) {
@@ -28,7 +26,6 @@ const checkDatabaseConnection = async (): Promise<void> => {
       await checkDatabaseConnection();
     } else {
       console.error('Max retries reached. Unable to connect to the database.');
-      return;
     }
   }
 };
@@ -36,17 +33,26 @@ const checkDatabaseConnection = async (): Promise<void> => {
 const startServer = async () => {
   await checkDatabaseConnection();
   await seed(); // Run the seed script
-  // The server start always
 
   const app = express();
+
+  // Trust the proxy to handle SSL offloading
+  app.set('trust proxy', true);
+
   const server = http.createServer(app);
-  const io = new Server(server, {cors: { origin: '*', credentials: true}});
+  const io = new Server(server, {
+    cors: {
+      origin: '*', // Allow all origins (adjust as needed for production)
+      credentials: true,
+    },
+    path: '/socket.io', // Ensure the WebSocket path is correct
+  });
 
   io.on('connection', (socket) => {
-    console.log('user connected');
+    console.log('User connected');
 
-    socket.on('disconnect', function () {
-      console.log('user disconnected');
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
     });
   });
 
